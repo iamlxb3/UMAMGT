19b079ac7c72af31b9ba42b2b43e8e04e4b4df26
iamlxb3 <iamlxb3@gmail.com>
2021-10-25 22:22:30 +0800

    1

--- app/exp_config.py
+++ app/exp_config.py
@@ -10,7 +10,7 @@ SYSTEM_CONFIG = {'per_device_train_batch_size': 32,
 # the frequency of evaluating on the val set for roberta
 TRAIN_CONFIG = {'epoch': 2,
                 'logging_steps': 20,
-                'eval_steps': 40}
+                'eval_steps': 50}
 
 TRAIN_DEBUG_CONFIG = {'epoch': 1,
                       'logging_steps': 1,
